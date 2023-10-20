30b9e7d28c57a854b7acdcde47bd9339fbc231a6
Miranda <flowerbutterfly8@gmail.com>
2023-10-20 13:45:59 -0400

    starting to work on a system to make armour designs programatically that can be 3D printed and sewn together

---
+++ customArmour/pattern.py
@@ -0,0 +1,5 @@
+"""
+This code handles the boundaries of the pattern, eventually this will 
+pull from an image file such that it can be any shape that is bounded, 
+but to start with it will just be a simple shape
+"""
---
+++ customArmour/pattern_to_model.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python
+
+from solid2 import *
+
+obj = cube(1,1,1)
+
+obj.save_as_scad("soupContainerLidStorage/soupLidHolder.scad")
